b94fbe2d0487fd3549e5994fb1440aa3287bf581
RanjithKumarS <ranjithkumar2002ranjis@gmail.com>
2024-04-17 20:30:34 +0530

    v1.2.8
    Changes:
    1. Some print statements and comments
    2. Increased MDSwitch hit-box size to match with its visual size
    
    Additions:
    1. New switch class (MySwitch)
    
    Fixed:
    1. Moving thumb icon with MDSwitch now patched

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import plyer
 
 import exceptions_handler
 
+from kivymd.uix.selectioncontrol import MDSwitch
 from kivymd.app import MDApp
 from kivymd.toast import toast as _toast
 from kivymd.uix.screen import MDScreen
@@ -62,7 +63,7 @@ fun_again_press = [
 
 error_handler = exceptions_handler.ErrorHandler('./Error log/')
 
-__version__ += " " * (bool(__test_version__)) + __test_version__  # Adds <space> if __test_version__ present
+__version__ += " " * (bool(__test_version__)) + __test_version__  # Adds <space> and version if __test_version__ present
 
 
 # Kivy doesn't allow changing screen from outer thread other than kivy's
@@ -255,6 +256,84 @@ def delete_log(_=None):
         pass
 
 
+class MySwitch(MDSwitch):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.is_thumb_size_changed = False
+
+    def on_touch_down(self, touch):
+        # print(dir(touch))
+        # print(self.pos, touch.pos, [self.size[0] + self.pos[0], self.size[1] + self.pos[1]])
+
+        # Not my code start
+        # IDK if it is needed but added from its parent class
+        if self.disabled and self.collide_point(*touch.pos):
+            return True
+        for child in self.children[:]:
+            if child.dispatch('on_touch_down', touch):
+                return True
+        # Not my code end
+
+        if self.ids['thumb'].collide_point(touch.pos[0], touch.pos[1]):
+            self.on_thumb_down()
+            self._update_thumb_pos()
+        # print(self.pos[1] + self.size[1] - touch.pos[1])
+
+    def on_touch_up(self, touch):
+        # Below line is copied from kivymd.uix.selectioncontrol.selectioncontrol.kv (line 84-97 kivymd version 1.0.2)
+        # This draws a line around switch, I used this as a new bigger hit-box
+        hit_box = (self.x - dp(2), self.center_y - dp(14), self.width + dp(14), dp(28)) if self.widget_style == "ios" \
+            else ((1, 1, 1, 1) if app.theme_cls.material_style == "M2" else
+                  (self.x + dp(8), self.center_y - dp(8), self.width + dp(16), dp(32)))
+
+        # Draws the hit box
+        # with self.canvas.after:
+        #     Color(rgba=(1, 1, 1, 1))
+        #     Line(points=[(a[0], a[1]),
+        #                  (a[0] + a[2], a[1]),
+        #                  (a[0] + a[2], a[1] + a[3]),
+        #                  (a[0], a[1] + a[3]),
+        #                  (a[0], a[1])
+        #                  ])
+
+        if (hit_box[0] <= touch.pos[0] <= hit_box[0] + hit_box[2] and
+                hit_box[1] <= touch.pos[1] <= hit_box[1] + hit_box[3]):
+            setattr(self, "active", not self.active)
+            self.is_thumb_size_changed = False
+            # print('activated')
+
+    def on_thumb_down(self) -> None:
+        """
+        Called at the on_touch_down event of the :class:`~Thumb` object.
+        Indicates the state of the switch "on/off" by an animation of
+        increasing the size of the thumb.
+        """
+
+        if self.widget_style != "ios" and self.theme_cls.material_style == "M3":
+            if self.active:
+                size = (dp(28), dp(28))
+                pos = (self.ids.thumb.pos[0] - dp(2), self.ids.thumb.pos[1] - dp(1.8),) if not self.is_thumb_size_changed \
+                    else self.ids.thumb.pos
+            else:
+                size = (dp(26), dp(26))
+                pos = (
+                    (
+                        self.ids.thumb.pos[0] - dp(5),
+                        self.ids.thumb.pos[1] - dp(5),
+                    )
+                    if not self.icon_inactive
+                    else (
+                        self.ids.thumb.pos[0] + dp(1),
+                        self.ids.thumb.pos[1] - dp(1),
+                    )
+                ) if not self.is_thumb_size_changed else self.ids.thumb.pos
+            Animation(size=size, pos=pos, t="out_quad", d=0.2).start(
+                self.ids.thumb
+            )
+            self.is_thumb_size_changed = True
+
+
 class WeeksToggleButtons(MDBoxLayout):
     weeks = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
 
@@ -411,7 +490,7 @@ class AlarmExpansionPanel(MDExpansionPanel):
                     self.panel_cls.height
         if min_items < len(self.parent.children):
             self.parent.parent.scroll_to(widget=self)
-        print(min_items)
+        # print(min_items)
         # print(self.height, self.content.height, self.panel_cls.height, app.root.height, home.ids['top_app_bar'].pos)
         Clock.schedule_once(self._modify_top_app_bar_color, 0.35)
 
@@ -546,7 +625,7 @@ class HomeScreen(MDScreen):
                     blocking everything except what we assign
         So we perform what was supposed to happen
         (Our main intention is to add directional scroll according to tab location,
-        which was previously unavailable due to the fact I explained in reason 1)
+        which was previously unavailable due to the fact that I explained in reason 1)
         NOTE: Everything is copied from kivymd/uix/bottomnavigation/bottomavigation.py
         basically this combines 2 functions
         """
@@ -614,11 +693,11 @@ class AddNewLocationScreen(MDScreen):
             # lat, lng = out[0], out[1]
             self.ids['cords_in'].error = not validate_gps_co_ords(txt)
         except KeyError:
-            print('Key')
+            print('Key error')
         except TypeError:
-            print('Type')
+            print('Type error')
         except ValueError:
-            print('Value')
+            print('Value error')
         else:
             # if no error
             return
